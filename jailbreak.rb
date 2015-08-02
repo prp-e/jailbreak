63e7649f30908b166a14ee06e273bcd9e26e2962
Muhammadreza Haghiri <haghiri75@gmail.com>
2015-08-02 17:22:56 +0430

    Some features added

--- jailbreak.rb
+++ jailbreak.rb
@@ -1,2 +1,26 @@
 class Jailbreak
+ def h1(txt)
+  return "<h1> #{txt} </h1>"
+ end
+ def h2(txt)
+  return "<h2> #{txt} </h2>"
+ end
+ def h3(txt)
+  return "<h3> #{txt} </h3>"
+ end
+ def h4(txt)
+  return "<h4> #{txt} </h4>"
+ end
+ def h5(txt)
+  return "<h5> #{txt} </h5>"
+ end
+ def h6(txt)
+  return "<h6> #{txt} </h6>"
+ end
+ def li(txt)
+  return "<li> #{txt} </li>"
+ end
+ def html(txt)
+  return txt
+ end
 end
